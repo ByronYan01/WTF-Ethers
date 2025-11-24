7009b6e711470645e9afeb6fd826880e1555de46
yanliang-code <1205836625@qq.com>
2025-11-24 20:28:38 +0800

    1-10节 provider 网络连接器 只读合约 转账eth 与链上合约交互 部署合约 查询事件信息、坚挺合约事件 事件过滤 单位转化 bigInt

--- 01_HelloVitalik/HelloVitalik.js
+++ 01_HelloVitalik/HelloVitalik.js
@@ -1,16 +1,25 @@
 // 导入ethers包
-import { ethers } from "ethers";
-// playcode免费版不能安装ethers，用这条命令，需要从网络上import包（把上面这行注释掉）
-// import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.2.3/ethers.js";
+import { ethers, AlchemyProvider } from "ethers";
 
 // 利用ethers默认的Provider连接以太坊网络
 // const provider = new ethers.getDefaultProvider();
-const ALCHEMY_MAINNET_URL = 'https://eth-mainnet.g.alchemy.com/v2/oKmOQKbneVkxgHZfibs-iFhIlIAl6HDN';
-const provider = new ethers.JsonRpcProvider(ALCHEMY_MAINNET_URL)
+// const ALCHEMY_MAINNET_URL = 'https://eth-mainnet.g.alchemy.com/v2/oKmOQKbneVkxgHZfibs-iFhIlIAl6HDN';
+// const provider = new ethers.JsonRpcProvider(ALCHEMY_MAINNET_URL)
+
+// 网络名称、api key
+// const provider = new AlchemyProvider("mainnet", "OqrhDyEGmnOqi2v76o7h9");
+
+// ENS服务仅在以太坊主网可用，测试网不支持
+// 如果一定要用测试网，下面执行 getBalance 时，需要传入地址
+const provider = new AlchemyProvider("sepolia", "OqrhDyEGmnOqi2v76o7h9");
 
 const main = async () => {
-    // 查询vitalik的ETH余额
-    const balance = await provider.getBalance(`vitalik.eth`);
-    // 将余额输出在console
-    console.log(`ETH Balance of vitalik: ${ethers.formatEther(balance)} ETH`);}
-main()
+  // 查询vitalik的ETH余额
+  //   const balance = await provider.getBalance(`vitalik.eth`);
+  const balance = await provider.getBalance(
+    `0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045`
+  );
+  // 将余额输出在console
+  console.log(`ETH Balance of vitalik: ${ethers.formatEther(balance)} ETH`);
+};
+main();
--- 10_Units/Units.js
+++ 10_Units/Units.js
@@ -1,28 +1,46 @@
 import { ethers } from "ethers";
 
+// 以太坊中，许多计算都对超出JavaScript整数的安全值（js中最大安全整数为9007199254740991）
+//因此，ethers.js使用 JavaScript ES2020 版本原生的 BigInt 类 安全地对任何数量级的数字进行数学运算
+
 // 1. BigNumber
-console.group('\n1. BigNumber类');
+console.group("\n1. BigNumber类");
 
 const oneGwei = ethers.getBigInt("1000000000"); // 从十进制字符串生成
-console.log(oneGwei)
-console.log(ethers.getBigInt("0x3b9aca00")) // 从hex字符串生成
-console.log(ethers.getBigInt(1000000000)) // 从数字生成
+console.log(oneGwei);
+console.log(ethers.getBigInt("0x3b9aca00")); // 从hex字符串生成
+console.log(ethers.getBigInt(1000000000)); // 从数字生成
 // 不能从js最大的安全整数之外的数字生成BigNumber，下面代码会报错
 // ethers.getBigInt(Number.MAX_SAFE_INTEGER);
-console.log("js中最大安全整数：", Number.MAX_SAFE_INTEGER)
+console.log("js中最大安全整数：", Number.MAX_SAFE_INTEGER);
 
 // 运算
-console.log("加法：", oneGwei + 1n)
-console.log("减法：", oneGwei - 1n)
-console.log("乘法：", oneGwei * 2n)
-console.log("除法：", oneGwei / 2n)
+// 注意：数值带后缀n会自动转换成BigInt
+console.log("加法：", oneGwei + 1n);
+console.log("减法：", oneGwei - 1n);
+console.log("乘法：", oneGwei * 2n);
+console.log("除法：", oneGwei / 2n);
 // 比较
-console.log("是否相等：", oneGwei == 1000000000n)
+console.log("是否相等：", oneGwei == 1000000000n);
 
+/**
+ * 在以太坊中，1 ether等于10^18 wei
+ * wei 0
+ * kwei 3
+ * mwei 6
+ * gwei 9
+ * szabo 12
+ * finney 15
+ * ether 18
+ *
+ * 用户可读单位：ether
+ * gas 价格可读单位：gwei
+ * 机器可读单位：wei
+ */
 
 // 2. 格式化：小单位转大单位
 // 例如将wei转换为ether：formatUnits(变量, 单位)：单位填位数（数字）或指定的单位（字符串）
-console.group('\n2. 格式化：小单位转大单位，formatUnits');
+console.group("\n2. 格式化：小单位转大单位，formatUnits");
 console.log(ethers.formatUnits(oneGwei, 0));
 // '1000000000'
 console.log(ethers.formatUnits(oneGwei, "gwei"));
@@ -37,10 +55,9 @@ console.log(ethers.formatEther(oneGwei));
 // `0.000000001` 等同于formatUnits(value, "ether")
 console.groupEnd();
 
-
-// 3. 解析：大单位转小单位
+// 3. 解析：大单位转小单位（返回 BigNumber 对象，最终都是转化为 wei）
 // 例如将ether转换为wei：parseUnits(变量, 单位),parseUnits默认单位是 ether
-console.group('\n3. 解析：大单位转小单位，parseUnits');
+console.group("\n3. 解析：大单位转小单位，parseUnits");
 console.log(ethers.parseUnits("1.0").toString());
 // { BigNumber: "1000000000000000000" }
 console.log(ethers.parseUnits("1.0", "ether").toString());
